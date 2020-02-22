b5df729deb201e5a9eca8d48c3dddcba9d5d175e
vonreuental <vonreuental@msn.com>
2020-02-22 23:16:42 +0800

    delete

--- ali/mat.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.figure()
-plt.plot([1, 0, 0], [4, 5, 6])
-plt.show()
--- zoj.py
+++
@@ -1,7 +0,0 @@
-while True:
-  input_msg = input('please input pairs of integers')
-  try:
-      a = input_msg.split()
-      print(int(a[0]) + int(a[1]))
-  except:
-      break
