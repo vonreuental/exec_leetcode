250f10e649f7c60b2715c053d0c2b3ccb7391906
vonreuental <vonreuental@msn.com>
2020-03-02 21:55:48 +0800

    commit

---
+++ 27.移除元素.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] == val:
+                nums.pop(i)
+        return len(nums)
+# @lc code=end
+
